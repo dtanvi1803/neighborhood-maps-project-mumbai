d2353fa1f75263b129633756509c2d2ff7518019
dtanvi1803 <dtanvi1803@yahoo.co.in>
2017-10-01 11:15:25 +0530

    Update app.js

--- neighborhood-maps-project-mumbai/js/app.js
+++ neighborhood-maps-project-mumbai/js/app.js
@@ -9,43 +9,10 @@ var markers = [];
 //3. create all markers with default color for all locations.
 function initMap() {
 
-    // Top places for Mumbai
-    var locations = [{
-            title: 'Gateway of India',
-            location: {
-                lat: 18.921984,
-                lng: 72.834654
-            }
-        },
-        {
-            title: 'Zoo',
-            location: {
-                lat: 18.978710,
-                lng: 72.835121
-            }
-        },
-        {
-            title: 'Jehangir Art Gallery',
-            location: {
-                lat: 18.927456,
-                lng: 72.831703
-            }
-        },
-        {
-            title: 'Essel World',
-            location: {
-                lat: 19.232724,
-                lng: 72.805508
-            }
-        },
-        {
-            title: 'Film Studio',
-            location: {
-                lat: 19.162903,
-                lng: 72.883690
-            }
-        }
-    ];
+    // Top places for Mumbai is stored in external file LocationsData. 
+    // The file is included in indext.html before app.js
+    // The variable locations is filled with this file.
+    //var locations = locationsData;
 
     // Create a styles array to use with the map.
     var styles = [{

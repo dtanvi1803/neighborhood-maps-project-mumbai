2d3e9aac35284c056c2cdfd44588c530988244cc
dtanvi1803 <dtanvi1803@yahoo.co.in>
2017-10-01 11:17:48 +0530

    LocationsData.js
    Locations data

---
+++ neighborhood-maps-project-mumbai/js/LocationsData.js
@@ -0,0 +1,36 @@
+    var locations = [{
+            title: 'Gateway of India',
+            location: {
+                lat: 18.921984,
+                lng: 72.834654
+            }
+        },
+        {
+            title: 'Zoo',
+            location: {
+                lat: 18.978710,
+                lng: 72.835121
+            }
+        },
+        {
+            title: 'Jehangir Art Gallery',
+            location: {
+                lat: 18.927456,
+                lng: 72.831703
+            }
+        },
+        {
+            title: 'Essel World',
+            location: {
+                lat: 19.232724,
+                lng: 72.805508
+            }
+        },
+        {
+            title: 'Film Studio',
+            location: {
+                lat: 19.162903,
+                lng: 72.883690
+            }
+        }
+    ]
